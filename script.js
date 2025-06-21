84088262196bc0a7c81a5942fbcf2268d39fe5ee
ddededodediamante <lukitadiamante5@gmail.com>
2025-06-21 17:13:56 -0300

    add zoom and more tags

--- script.js
+++ script.js
@@ -106,6 +106,14 @@ const workspace = Blockly.inject("blocklyDiv", {
   trashcan: true,
   renderer: "zelos",
   theme: savedTheme === "dark" ? darkTheme : lightTheme,
+  zoom: {
+    controls: true, 
+    wheel: true, 
+    startScale: 1,
+    maxScale: 3, 
+    minScale: 0.3, 
+    scaleSpeed: 1.2, 
+  },
 });
 
 toggleBtn.addEventListener("click", () => {
