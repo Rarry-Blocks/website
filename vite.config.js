705ef449c51c9fb158f603448aea15c7322936a5
ddededodediamante <lukitadiamante5@gmail.com>
2025-11-01 20:23:15 -0300

    bro is

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-export default {
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        editor: resolve(__dirname, 'editor.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        user: resolve(__dirname, 'user.html'),
+      },
       treeshake: false,
     },
   },
-};
+});
